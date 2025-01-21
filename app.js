ed19238e326e038728ba2e346568e9dbcf6dd93d
Samuel Salvador <samuel20018@gmail.com>
2025-01-21 13:18:20 -0300

    desafio completo

---
+++ app.js
@@ -0,0 +1,32 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let amigoInserido = document.getElementById("amigo").value;
+
+  if (amigoInserido == "") {
+    alert("Por favor, insira um nome.");
+  } else amigos.push(amigoInserido);
+
+  document.getElementById("amigo").value = "";
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
+
+let listaAmigos = document.getElementById("listaAmigos");
+
+function sortearAmigo() {
+  let amigoSorteado = "";
+  if (amigos.length > 0) {
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    amigoSorteado = amigos[indiceSorteado];
+    console.log(indiceSorteado);
+  }
+  document.getElementById("resultado").innerHTML = amigoSorteado;
+  document.querySelector(".button-draw").innerHTML =
+    '<img src="assets/play_circle_outline.png" alt="Ícone para sortear">Sortear outro amigo';
+}
